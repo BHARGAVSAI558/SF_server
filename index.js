41ff7104222b15d97727a374a88b1affdfcbdc27
BHARGAVSAI523 <bhargavsai55555t@gmail.com>
2024-11-12 23:21:55 +0530

    Initial commit for backend

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
